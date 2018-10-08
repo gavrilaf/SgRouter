1554b8c99ebbb7b26b2480f7e3b84f76a757a81a
eugenf <eugenf78@gmail.com>
2018-10-08 12:44:27 +0300

    renamed PerfTest -> RouterPerfTest

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     name: "SgRouter",
     products: [
         .library(name: "SgRouter", targets: ["SgRouter"]),
-        .executable(name: "PerfTest", targets: ["PerfTest"]),
+        .executable(name: "RouterPerfTest", targets: ["RouterPerfTest"]),
     ],
     dependencies: [
         .package(url: "https://github.com/gavrilaf/SwiftPerfTool.git", from: "0.1.0"),
     ],
     targets: [
         .target(name: "SgRouter", dependencies: []),
-        .target(name: "PerfTest", dependencies: ["SgRouter", "SwiftPerfTool"], path: "Sources/Examples/PerfTest"),
+        .target(name: "RouterPerfTest", dependencies: ["SgRouter", "SwiftPerfTool"], path: "Sources/Examples/RouterPerfTest"),
         .testTarget(name: "SgRouterTests", dependencies: ["SgRouter"]),
     ]
 )
